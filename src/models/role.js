c3d77c6060abcc62dd3342a02cc4fa4732a55a0e
Sanyam Goyal <sanyamgoyal2859@gmail.com>
2023-07-21 20:27:24 +0530

    added models for many to many association between roles and users

---
+++ src/models/role.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+const {ENUMS} = require('../utils/common');
+const {ADMIN, CUSTOMER, FLIGHT_COMPANY} = ENUMS
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsToMany(models.User, {through: 'User_Roles', as:'user'})
+    }
+  }
+  Role.init({
+    name: DataTypes.STRING,
+    values: [ADMIN, CUSTOMER, FLIGHT_COMPANY]
+  }, {
+    sequelize,
+    modelName: 'Role',
+  });
+  return Role ;
+};
